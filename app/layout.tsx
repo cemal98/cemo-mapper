821b34eb356d62f2808904fa52359e0d63b84c6a
cemalkilickaya <43484779+cemalkilickaya@users.noreply.github.com>
2024-07-31 17:48:18 +0300

    reset

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Component } from "react";
 
 const FontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -23,10 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn("min-h-screen font-sans antialiased", FontSans.variable)}
-      >
-        {children}
+      <body>
+        <ChakraProvider>{children}</ChakraProvider>
       </body>
     </html>
   );
