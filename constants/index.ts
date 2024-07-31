eba05656414e0c9dc884d25328f6bb641fd9d08d
cemalkilickaya <43484779+cemalkilickaya@users.noreply.github.com>
2024-07-31 17:09:27 +0300

    sidebar-navlinks

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const navLinks = [
+  {
+    label: "Home",
+    route: "/",
+    icon: "/assets/icons/home.svg",
+  },
+  {
+    label: "Add Location",
+    route: "/locations/add",
+    icon: "/assets/icons/add-location.svg",
+  },
+  {
+    label: "List Locations",
+    route: "/locations/list",
+    icon: "/assets/icons/list-locations.svg",
+  },
+  {
+    label: "Edit Location",
+    route: "/locations/edit",
+    icon: "/assets/icons/edit-location.svg",
+  },
+  {
+    label: "Map View",
+    route: "/map",
+    icon: "/assets/icons/map.svg",
+  },
+];
